768d4504456599807dd70008d668e9774c5bc07e
MatheusRodriguesBezerra <matheusrodbez@gmail.com>
2023-07-24 18:06:25 -0300

    main page done and img uploading

--- webtv/src/components/main.js
+++ webtv/src/components/main.js
@@ -1,34 +1,33 @@
+import { useEffect, useState } from 'react';
+import 'firebase/compat/firestore';
+import firebase from '../db';
+
 export function MainGrid() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const db = firebase.firestore();
+            const dataRef = db.collection('streams');
+            const snapshot = await dataRef.get();
+            const documents = snapshot.docs.map((doc) => doc.data());
+            setData(documents);
+        };
+
+        fetchData();
+    }, []);
+
     return(
         <main>
             <div class="grid-container">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+            {data.map((item) => (
+                <div class="card">
+                    <img src={ item.img } alt={ item.alt }/>
+                    <h2>{ item.Title }</h2>
+                    <p>{ item.Description }</p>
+                </div>
+            ))}
             </div>
         </main>
-    );
-}
-
-export function Card() {
-    return(
-        <div class="card">
-            <img src="caminho/para/imagem1.jpg" alt="Imagem do Card 1"/>
-            <h2>Card 1</h2>
-            <p>Breve descrição do Card 1...</p>
-        </div>
     );
 }
